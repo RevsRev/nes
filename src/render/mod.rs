8049386484b9b4f118326e00e24c3532a04d4b80
RevsRev <eddie.revell@yahoo.co.uk>
2025-08-05 21:17:25 +0100

    Render background tiles with the correct colour

--- src/render/mod.rs
+++ src/render/mod.rs
@@ -6,38 +6,6 @@ use crate::ppu::PPU;
 pub mod frame;
 pub mod palette;
 
-pub fn show_tile(frame: &mut Frame, chr_rom: &Vec<u8>, bank: usize, tile_n: usize) {
-    assert!(bank <= 1);
-
-    let bank = (bank * 0x1000) as usize;
-
-    let tile = &chr_rom[(bank + tile_n * 16)..=(bank + tile_n * 16 + 15)];
-
-    let col = tile_n % 32;
-    let row = tile_n / 32;
-
-    for y in 0..=7 {
-        let mut upper = tile[y];
-        let mut lower = tile[y + 8];
-
-        for x in (0..=7).rev() {
-            //There are 4 "colours": 00, 01, 10 & 11
-            let value = (0b1 & upper) << 1 | (0b1 & lower);
-            upper = upper >> 1;
-            lower = lower >> 1;
-            let rgb = match value {
-                0 => palette::SYSTEM_PALLETE[0x01],
-                1 => palette::SYSTEM_PALLETE[0x23],
-                2 => palette::SYSTEM_PALLETE[0x27],
-                3 => palette::SYSTEM_PALLETE[0x30],
-                _ => panic!("Impossible!"),
-            };
-            frame.set_pixel(col * 8 + x, row * 8 + y, rgb);
-            // frame.set_pixel(x, y, rgb);
-        }
-    }
-}
-
 pub fn render(frame: &mut Frame, ppu: &PPU) {
     let bank = ppu.ctl.bknd_pattern_addr();
 
@@ -48,6 +16,7 @@ pub fn render(frame: &mut Frame, ppu: &PPU) {
         let row = i / 32;
 
         let tile = &ppu.chr_rom[(bank + tile * 16) as usize..=(bank + tile * 16 + 15) as usize];
+        let pallette = background_pallette(ppu, col, row);
 
         for y in 0..8 {
             let mut upper = tile[y];
@@ -58,14 +27,35 @@ pub fn render(frame: &mut Frame, ppu: &PPU) {
                 upper = upper >> 1;
                 lower = lower >> 1;
                 let rgb = match value {
-                    0 => SYSTEM_PALLETE[0x01],
-                    1 => SYSTEM_PALLETE[0x23],
-                    2 => SYSTEM_PALLETE[0x27],
-                    3 => SYSTEM_PALLETE[0x30],
+                    0 => SYSTEM_PALLETE[ppu.palette_table[0] as usize],
+                    1 => SYSTEM_PALLETE[pallette[1] as usize],
+                    2 => SYSTEM_PALLETE[pallette[2] as usize],
+                    3 => SYSTEM_PALLETE[pallette[3] as usize],
                     _ => panic!("Impossible!"),
                 };
                 frame.set_pixel(col * 8 + x, row * 8 + y, rgb);
             }
         }
     }
+}
+
+fn background_pallette(ppu: &PPU, tile_column: usize, tile_row: usize) -> [u8; 4] {
+    let attr_table_index = 8 * (tile_row / 4) + tile_column / 4;
+    let attr_byte = ppu.vram[0x03C0 + attr_table_index]; //hard coded first nametable
+
+    let pallette_index = match ((tile_column % 4) / 2, (tile_row % 4) / 2) {
+        (0, 0) => attr_byte & 0b11,
+        (1, 0) => (attr_byte >> 2) & 0b11,
+        (0, 1) => (attr_byte >> 4) & 0b11,
+        (1, 1) => (attr_byte >> 6) & 0b11,
+        (_, _) => panic!("Impossible!"),
+    };
+
+    let pallette_start: usize = 1 + (pallette_index as usize) * 4;
+    [
+        ppu.palette_table[0],
+        ppu.palette_table[pallette_start],
+        ppu.palette_table[pallette_start + 1],
+        ppu.palette_table[pallette_start + 2],
+    ]
 }

304567d665ff95ac51b7741cc7a2c370ecf66b9a
RevsRev <eddie.revell@yahoo.co.uk>
2026-02-28 21:55:37 +0000

    Try averaging over values to get the audio more smooth (but it isn't)

--- src/io/audio/sound_frame.rs
+++ src/io/audio/sound_frame.rs
@@ -1,17 +1,31 @@
 pub struct SoundFrame {
-    output: f32,
+    last_output: f32,
+    sample_sum: f32,
+    sample_count: f32,
 }
 
 impl SoundFrame {
     pub fn new() -> Self {
-        SoundFrame { output: 0.0 }
+        SoundFrame {
+            last_output: 0.0,
+            sample_sum: 0.0,
+            sample_count: 0.0,
+        }
     }
 
     pub fn get_output(&mut self) -> f32 {
-        self.output
+        if self.sample_count == 0.0 {
+            return self.last_output;
+        }
+        self.last_output = 3.0 * self.sample_sum / self.sample_count;
+        self.sample_sum = 0.0;
+        self.sample_count = 0.0;
+
+        self.last_output
     }
 
     pub fn add_output(&mut self, output: f32) {
-        self.output = output;
+        self.sample_sum += output;
+        self.sample_count += 1.0;
     }
 }
